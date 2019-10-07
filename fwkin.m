f137a7a1f2bc4eced8e48a3fac18190e2c31a195
Anqi Shen <anqishen1997@gmail.com>
2019-10-06 21:41:48 -0400

    fix DH paramters

--- fwkin.m
+++ fwkin.m
@@ -1,11 +1,11 @@
 function joint_positions = fwkin(q1,q2,q3,q4,q5)
 % angles in rad
-    l1 = 4.123; % in
+    l1 = 4.125; % in
     l2 = 6.43; % in
     
 
     T01 = fwkintrans(l1,q1,0,-3.1415/2);
-    T02 = T01 * fwkintrans(0,q2,l2,0);
+    T02 = T01 * fwkintrans(0,q2-3.1415/2,l2,0);
     T03 = T02 * fwkintrans(0,q3,l2,0);
     T04 = T03 * fwkintrans(0,q4,l1,3.1415/2);
     T05 = T04 * fwkintrans(0,q5,0,0);
