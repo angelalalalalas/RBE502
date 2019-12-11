ce178ce5b4187fd6bba2ab12f31baa0debb3f224
Anqi Shen <anqishen1997@gmail.com>
2019-12-11 09:31:41 -0500

    modify quintic trajectory

--- quinticTrajectory.m
+++ quinticTrajectory.m
@@ -1,11 +1,11 @@
 function coeffMat = quinticTrajectory(ti, tf, pi, pf, vi, vf, ai, af)
         % The matrix containing the terms of the constraint equations
-        quinticMat = [     ti^5,      ti^4,     ti^3, ti^2, ti, 1;
-                       5*(ti^4),  4*(ti^3), 3*(ti^2), 2*ti,  1, 0;
-                      20*(ti^3), 12*(ti^2),     6*ti,    2,  0, 0;
-                           tf^5,      tf^4,     tf^3, tf^2, tf, 1;
-                       5*(tf^4),  4*(tf^3), 3*(tf^2), 2*tf,  1, 0;
-                      20*(tf^3), 12*(tf^2),     6*tf,    2,  0, 0];
+        quinticMat = [ 1, ti, ti^2,     ti^3,      ti^4,      ti^5;
+                       0,  1, 2*ti, 3*(ti^2),  4*(ti^3),  5*(ti^4);
+                       0,  0,    2,     6*ti, 12*(ti^2), 20*(ti^3);
+                       1, tf, tf^2,     tf^3,      tf^4,      tf^5;
+                       0,  1, 2*tf, 3*(tf^2),  4*(tf^3),  5*(tf^4);
+                       0,  0,    2,     6*tf, 12*(tf^2), 20*(tf^3)];
         % This matrix contains the initial and end conditions
         initEndMat = [pi; vi; ai; pf; vf; af];
 
